bdc077e6f5b33d0463fc10fcdc84bc922b7d8f57
Abdur Siddiqui <abdursiddiqui2003@gmail.com>
2022-12-25 00:28:21 -0500

    Modifying app.js

--- src/App.js
+++ src/App.js
@@ -8,11 +8,6 @@ function App() {
 <header> 
   Real Estate Booking</header>
 
-  <form class="example" action="/action_page.php">
-  <input type="text" placeholder="Search for a home" name="search"/>
-  <button type="submit"><i class="fa fa-search"></i></button>
-</form>
-
 
 
     </div>
